353f592cb1c8f9964f4438fbc3dfc5eb688a1912
Liau Chuan Yi <liaucyi@gmail.com>
2020-10-12 00:07:55 +0800

    Initial commit
    Setup working discord.js bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const config = require('./config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Bot token should always be placed in config.json and never committed to repo
+client.login(config.token);
