b4169fb8bf2f4a3a03e6399c6c44377ba0bb09d8
Liau Chuan Yi <liaucyi@gmail.com>
2020-10-15 22:54:07 +0800

    Use empty embed field for spacing

--- src/commands/gw/pre-searing.ts
+++ src/commands/gw/pre-searing.ts
@@ -7,6 +7,16 @@ import {
     getActivityMeta,
 } from '../../lib/activities';
 
+/**
+ * A blank field to create spacing between embed fields.
+ * The markdown syntax is necessary because Discord ignores whitespace.
+ */
+const BLANKFIELD = {
+    name: '**    **',
+    value: '**    **',
+    inline: true,
+};
+
 export = class PreSearingCommand extends Command {
     constructor(client: CommandoClient) {
         super(client, {
@@ -22,26 +32,21 @@ export = class PreSearingCommand extends Command {
     async run(message: CommandoMessage) {
         const now = new Date();
 
-        /**
-         * Create spacing between embed fields.
-         * This is necessary because Discord automatically removes normal spaces.
-         */
-        const blankSpace = '** ** ** ** ** **';
-
         const output = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Pre Searing Quests')
             .setURL('https://wiki.guildwars.com/wiki/Daily_activities/')
             .addFields(
                 {
-                    name: `Vanguard Quest${blankSpace}||${blankSpace}`,
+                    name: `Vanguard Quest`,
                     value: 
                         `${getActivity('vanguard', now)}\n` + 
                         `_${getActivityMeta('vanguard', now)['dailyCountdown']} left_`,
                     inline: true,
                 },
+                BLANKFIELD,
                 {
-                    name: `Nicholas Sanford`,
+                    name: `Nicholas Sandford`,
                     value: 
                         `${getActivity('nicholas-sandford', now)}\n` + 
                         `_${getActivityMeta('nicholas-sandford', now)['dailyCountdown']} left_`,
