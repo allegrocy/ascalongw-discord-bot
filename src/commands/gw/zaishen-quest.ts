8ef4a1b2de7defe547be3ce6869c356d4a2f172f
Liau Chuan Yi <liaucyi@gmail.com>
2020-10-15 23:04:36 +0800

    Use embeds for zaishen-quest command

--- src/commands/gw/zaishen-quest.ts
+++ src/commands/gw/zaishen-quest.ts
@@ -1,10 +1,21 @@
 import { intervalToDuration } from 'date-fns';
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
+import Discord from 'discord.js';
 
 import {
     getActivity,
     getActivityMeta,
 } from '../../lib/activities';
+
+/**
+ * A blank field to create spacing between embed fields.
+ * The markdown syntax is necessary because Discord ignores whitespace.
+ */
+const BLANKFIELD = {
+    name: '**    **',
+    value: '**    **',
+    inline: true,
+};
 
 export = class ZaishenQuestCommand extends Command {
     constructor(client: CommandoClient) {
@@ -22,17 +33,43 @@ export = class ZaishenQuestCommand extends Command {
         const { dailyCountdown } = getActivityMeta('zaishen-mission');
         const now = new Date();
 
-        const zaishenQuestsText = (date: Date) => [
-            `Zaishen Mission: **${getActivity('zaishen-mission', date)}**`,
-            `Zaishen Bounty: **${getActivity('zaishen-bounty', date)}**`,
-            `Zaishen Vanquish: **${getActivity('zaishen-vanquish', date)}**`,
-            `Zaishen Combat: **${getActivity('zaishen-combat', date)}**`,
-        ];
+        const output = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Zaishen Quests')
+            .setURL('https://wiki.guildwars.com/wiki/Zaishen_Challenge_Quest')
+            .addFields(
+                {
+                    name: `Zaishen Mission`,
+                    value: 
+                        `${getActivity('zaishen-mission', now)}\n` + 
+                        `_${getActivityMeta('zaishen-mission', now)['dailyCountdown']} left_`,
+                    inline: true,
+                },
+                BLANKFIELD,
+                {
+                    name: `Zaishen Mission`,
+                    value: 
+                        `${getActivity('zaishen-bounty', now)}\n` + 
+                        `_${getActivityMeta('zaishen-bounty', now)['dailyCountdown']} left_`,
+                    inline: true,
+                },
+                {
+                    name: `Zaishen Vanquish`,
+                    value: 
+                        `${getActivity('zaishen-vanquish', now)}\n` + 
+                        `_${getActivityMeta('zaishen-vanquish', now)['dailyCountdown']} left_`,
+                    inline: true,
+                },
+                BLANKFIELD,
+                {
+                    name: `Zaishen Combat`,
+                    value: 
+                        `${getActivity('zaishen-combat', now)}\n` + 
+                        `_${getActivityMeta('zaishen-combat', now)['dailyCountdown']} left_`,
+                    inline: true,
+                },
+            );
 
-        return message.say([
-            '__Today\'s Zaishen Quests:__',
-            ...zaishenQuestsText(now),
-            `Zaishen Daily Quest will reset in ${dailyCountdown}`,
-        ]);
+        return message.say(output);
     }
 }
