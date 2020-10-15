391c3d198b394f453b02f7ea3e66c06fad4df6d6
Liau Chuan Yi <liaucyi@gmail.com>
2020-10-15 11:01:19 +0800

    Disable eval command

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,7 @@ client.registry
     .registerDefaultTypes()
     .registerDefaultGroups()
     .registerDefaultCommands({
+        eval: false,
         commandState: false
     })
     .registerCommandsIn({
