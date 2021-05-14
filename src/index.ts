5e731e26e49dd88683b050c63cae0f41a3ef859c
Jon <>
2021-05-14 11:14:03 +0100

    Tweaked config to allow single server to run multiple bots if needed

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,56 @@ import config from '../config.json';
 import { addUncachedMessageReactionHandler } from './helper/reaction';
 import permissions from './helper/permissions';
 
-const client = new CommandoClient({
-    commandPrefix: config.prefix,
-    owner: config.owners
-});
+var myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
 
-client.once('ready', async () => {
-    if (!client.user) return;
-    console.log(`Ready! Logged in as ${client.user.username}#${client.user.discriminator}.`);
+if(config.tokens)
+  config.tokens.forEach(startClient);
+else
+  startClient(config.token);
 
-    console.log(await client.generateInvite(permissions));
-    addUncachedMessageReactionHandler(client);
-    client.user.setPresence({ activity: { name: '-help', type:'LISTENING' }});
-});
+function startClient(token: string) {
+  token = token.trim();
+  if(!token)
+    return;
+  const client = new CommandoClient({
+      commandPrefix: config.prefix,
+      owner: config.owners
+  });
 
-client.registry
-    .registerGroups([
-        ['gw', 'Guild Wars-related commands']
-    ])
-    .registerDefaultTypes()
-    .registerDefaultGroups()
-    .registerDefaultCommands({
-        eval: false,
-        unknownCommand: false,
-        commandState: false
-    })
-    .registerCommandsIn({
-        dirname: path.join(__dirname, 'commands'),
-        filter: /(.+)\.(js|ts)$/,
-    });
+  client.once('ready', async () => {
+      if (!client.user) return;
+      console.log(`Ready! Logged in as ${client.user.username}#${client.user.discriminator}.`);
 
-// Bot token should always be placed in config.json and never committed to repo
-console.log("Bot token is "+config.token);
-client.login(config.token);
+      console.log(await client.generateInvite(permissions));
+      addUncachedMessageReactionHandler(client);
+      client.user.setPresence({ activity: { name: '-help', type:'LISTENING' }});
+  });
+
+  client.registry
+      .registerGroups([
+          ['gw', 'Guild Wars-related commands']
+      ])
+      .registerDefaultTypes()
+      .registerDefaultGroups()
+      .registerDefaultCommands({
+          eval: false,
+          unknownCommand: false,
+          commandState: false
+      })
+      .registerCommandsIn({
+          dirname: path.join(__dirname, 'commands'),
+          filter: /(.+)\.(js|ts)$/,
+      });
+
+  // Bot token should always be placed in config.json and never committed to repo
+  console.log("Bot token is "+token);
+  client.login(token);
+
+  setInterval(function() {
+    try {
+      if (global.gc) {global.gc();}
+    } catch(e) { }
+  },60000);
+
+}
