61ba2e0b08edcf5fc4038c88696da59c6037b87e
Jon <>
2023-01-08 09:11:27 +0000

    added -gww command

--- src/commands/gw/wiki.ts
+++ src/commands/gw/wiki.ts
@@ -1,26 +1,18 @@
 import { Command, CommandoClient, CommandoMessage } from 'discord.js-commando';
 import axios from 'axios';
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
-const trade_website = 'https://kamadan.gwtoolbox.com';
-const max_results = 10;
+const wiki_website = 'https://wiki.guildwars.com';
 
-interface SearchEntry {
-    t: number;
-    s: string;
-    m: string;
-}
-
-export default class PricecheckCommand extends Command {
+export default class WikiCommand extends Command {
     constructor(client: CommandoClient) {
         super(client, {
-            name: 'pricecheck',
-            aliases: ['pc'],
+            name: 'wiki',
             group: 'gw',
-            memberName: 'pc',
-            description: `Queries ${trade_website} for trade results.`,
+            aliases:['gww','guildwarswiki'],
+            memberName: 'wiki',
+            description: `Queries Guild Wars Wiki for a search term.`,
             details: '',
-            examples: ['pc q9 torm shield'],
+            examples: ['wiki Duke Barradin'],
             args: [
                 {
                     key: 'search',
@@ -37,30 +29,15 @@ export default class PricecheckCommand extends Command {
         const sayResult = await message.say(`Searching for **${args.search}**, just a sec...`);
         const searchMessage = Array.isArray(sayResult) ? sayResult[sayResult.length - 1] : sayResult;
 
-        const enc_search = encodeURIComponent(args.search);
-        const response = await axios.get<SearchEntry>(`${trade_website}/s/${enc_search}`);
+        // NB: Wiki turns spaces into underscores, but idk if thats needed here
+        const enc_search = encodeURIComponent(args.search.toLowerCase());
+        const url = `${wiki_website}/index.php?search=${enc_search}`;
 
-        if (response.status !== 200) {
-            return searchMessage.edit(`Sorry, something went wrong fetching results from ${trade_website}.`);
+        try {
+            const response = await axios.get(url);
+            return await searchMessage.edit(response.request.res.responseUrl);
+        } catch(e) {
+            return await searchMessage.edit(`Sorry, something went wrong fetching results from ${url}.`);
         }
-
-        const json = response.data;
-        if (!Array.isArray(json) || !json.length) {
-            return searchMessage.edit(`No results found for **${args.search}**`);
-        }
-
-        const results = json.map(data => {
-            const sender = data.s.padStart(20, ' ');
-            const time = formatDistanceToNow(new Date(data.t), { addSuffix: true });
-            const prefix = `${sender}, ${time}:`.padEnd(42, ' ');
-            return `${prefix} ${data.m}`;
-        });
-
-        const shownResults = results.slice(0, max_results);
-
-        return await searchMessage.edit([
-            `Latest ${shownResults.length} results for **${args.search}** from ${trade_website}/search/${enc_search}`,
-            `\`\`\`${shownResults.join('\n')}\`\`\``,
-        ]);
     }
 }
